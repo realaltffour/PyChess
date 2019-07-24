f467c35f2c36868c039a89659ca757d59afa0738
ayhammamoun <ayhaboualfadl@gmail.com>
2019-07-24 13:21:56 +0300

    Added __init__.py - Added setup.py data

--- PyChess/data/pieces.py
+++ PyChess/data/pieces.py
@@ -1,4 +1,9 @@
 import sys
 
-import pieces
+import pieces.King
+import pieces.Queen
+import pieces.Rook
+import pieces.Bishop
+import pieces.Knight
+import pieces.Pawn
 import Piece
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='PyChess',
+   version='1.0',
+   description='A Chess Game Written In Python.',
+   author='Ayham Mamoun ft. Ali',
+   author_email='ayhamaboualfadl@gmail.com',
+   packages=['PyChess']
+)
